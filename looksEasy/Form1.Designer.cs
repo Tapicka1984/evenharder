4a565fb6e08e86bab8333e45d0e2dbc9272331b7
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2023-09-12 17:05:07 +0200

    Add files via upload

---
+++ looksEasy/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace looksEasy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(135, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "pocet cisel - muze byt do 20";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(107, 342);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(138, 87);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(107, 342);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ looksEasy/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace looksEasy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            try
+            {
+                if (textBox1.Text != String.Empty)
+                {
+                    int cislo = int.Parse(textBox1.Text);
+                    if (cislo > 20)
+                    {
+                        MessageBox.Show("cislo too big");
+                    }
+                    else
+                    {
+                        Random r = new Random();
+                        int[] cisla = new int[cislo];
+                        int x = 0;
+                        while (x < cislo)
+                        {
+                            cisla[x] = r.Next(-1, 25);
+                            x++;
+                        }
+                        foreach (int i in cisla)
+                        {
+                            listBox1.Items.Add(i);
+                        }
+                        PoslPrvočíslo(cisla);
+                        Vymen(cisla, cislo);
+                        Vypis(cisla);
+                    }
+                }
+            }
+            catch(Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void PoslPrvočíslo(int[] cisla)
+        {
+            int posledniprvocislo = 0;
+            int pozice = 1;
+            int poziceposledniho = 0;
+            foreach (int cislo in cisla)
+            {
+                if (cislo < 2)
+                {
+                    continue;
+                }
+
+                bool jePrvocislo = true;
+
+                for (int i = 2; i * i <= cislo; i++)
+                {
+                    if (cislo % i == 0)
+                    {
+                        jePrvocislo = false;
+                        break; 
+                    }
+                }
+
+                if (jePrvocislo == true)
+                {
+                    posledniprvocislo = cislo;
+                    poziceposledniho = pozice;
+                }
+                pozice++;
+            }
+            if (posledniprvocislo == 0)
+            {
+                MessageBox.Show("pole neobsahuje prvocisla");
+            }
+            else
+            {
+                MessageBox.Show("posledni prvocislo je: " + posledniprvocislo + ", jeho pozice je: " + poziceposledniho);
+            }
+        }
+
+        private void Vymen(int[] cisla, int cislo)
+        {
+            int prvni = cisla[0];
+            int posledni = cisla[cislo - 1];
+            cisla[0] = posledni;
+            cisla[cislo - 1] = prvni;
+        }
+
+        private void Vypis(int[] cisla)
+        {
+            foreach(int i in cisla)
+            {
+                listBox2.Items.Add(i);
+            }
+        }
+            
+    }
+}
